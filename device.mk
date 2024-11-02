c16aa2099602be3fc41ed97aefed8f5298c3ef66
LuK1337 <priv.luk@gmail.com>
2024-11-02 12:45:07 +0100

    jasmine_sprout: Build libhidlbase-v32.vendor
    Change-Id: I1b31199ec389ea6ba0d8faea4c539839c1cb8170

--- device.mk
+++ device.mk
@@ -53,8 +53,8 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+PRODUCT_PACKAGES += \
+    libhidlbase-v32.vendor
 
 # Wifi
 PRODUCT_PACKAGES += \
