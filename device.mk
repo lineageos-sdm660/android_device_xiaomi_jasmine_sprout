b7189ad481bf64c2dfce1fff0c14618d2057d79a
Arian <arian.kulmer@web.de>
2026-09-25 01:25:12 +0200

    jasmine_sprout: Build fingerprint from source
    Signed-off-by: Onelots <onelots@lineageos.org>

--- device.mk
+++ device.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    com.fingerprints.extension@1.0.vendor
+    com.fingerprints.extension@1.0.vendor \
+    vendor.goodix.hardware.fingerprint@1.0.vendor
 
 # Init
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -11,10 +11,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
             ;;
-        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-            ;;
         *)
             return 1
             ;;
